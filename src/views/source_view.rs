d129dbaa7be857e7657c2ba604db4ad788f41805
Sneha Roy <sneharoym81@gmail.com>
2026-06-05 12:11:58 -0400

    create text editor widget for assembly code input

--- src/views/source_view.rs
+++ src/views/source_view.rs
@@ -1,6 +1,15 @@
 use eframe::egui;
 
-pub fn draw(ui: &mut egui::Ui, source_text: &str) {
+pub fn draw_editor(ui: &mut egui::Ui, source_text: &mut String) {
     ui.heading("Program");
-    ui.monospace(source_text); // monospace: character is the same widt
+    
+    //  egui creates a text edit widget
+    egui::ScrollArea::vertical().show(ui, |ui| {
+        ui.add(
+            egui::TextEdit::multiline(source_text)
+                .font(egui::TextStyle::Monospace)
+                .desired_width(f32::INFINITY)
+                .desired_rows(30)
+        );
+    });
 } 
