74e633b75100cacf46bd59b84691cc90786e1949
Sneha Roy <sneharoym81@gmail.com>
2026-06-05 12:11:42 -0400

    show register values in both hexadecimal and decimal format

--- src/views/register_view.rs
+++ src/views/register_view.rs
@@ -2,23 +2,21 @@ use eframe::egui;
 use crate::cpu::CPU;
 
 pub fn draw(ui: &mut egui::Ui, cpu: &CPU) {
-
     ui.heading("Registers");
     let registers = cpu.view_registers();
 
     for i in 0..8 {
         let value = registers[i];
 
-        // display: register name, value in hex, value as a signed number
         ui.label(format!(
-            "R{}    x{:04X}    {}",
+            "R{}    x{:04X}    {}",                     // display: {register name}, {hex value}, {signed decimal number}
             i, value, value as i16
         ));
     }
 
-    ui.separator();     // draw a dividing line between registers and PC
+    ui.separator();    
 
-    // show the program counter
+    // show the pc
     let pc = cpu.view_pc();
     ui.label(format!(
         "PC    x{:04X}    {}", 
